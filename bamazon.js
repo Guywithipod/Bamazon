3b765aafc4b853bbce67ae95dfbae3d8fcd99b27
Anthony Dominguez <Anthonydv7@outlook.com>
2019-09-18 17:06:31 -0500

    app is now MVP

--- bamazon.js
+++ bamazon.js
@@ -2,7 +2,7 @@ const inquirer = require("inquirer");
 
 const mysql = require("mysql");
 
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
@@ -12,37 +12,64 @@ var connection = mysql.createConnection({
 
 connection.query("SELECT item_id, product_name, price FROM products", function (err, res) {
   if (err) throw err;
-  console.log(res);
+  for (let i = 0; i < res.length; i++) {
+    console.log("Item ID:" + res[i].item_id + "     Product Name:" + res[i].product_name + "        Price:" + res[i].price);
 
-  
-inquirer.prompt([
-  {
-    name: "name",
-    message: "What is this ID of the item you wish to purchuse?"
-  },
+  }
 
-  {
-    name: "units",
-    message: "How many units do you wish to purchuse?"
-  },
-]).then(function (answers) {
+  inquirer.prompt([
+    {
+      name: "name",
+      message: "What is this ID of the item you wish to purchuse?"
+    },
 
-  readtable(answers)
-});
-});
+    {
+      name: "units",
+      message: "How many units do you wish to purchuse?"
+    },
+  ]).then(function (answers) {
 
-function readtable(answers) {
-  connection.query("SELECT price, product_name FROM products", function (err, res) {
-    if (err) throw err;
-
-    if (answers.units < 1) {
-      console.log("Insufficient quantity!");
-    };
-
-    connection.end();
+    readtable(answers)
   });
-}
 
-// const updateTable = () => {
 
-// }
+  function readtable(answers) {
+    connection.query("SELECT item_id FROM products", function (err, res) {
+      if (err) throw err;
+
+      const Id = answers.name
+      console.log(Id)
+
+      connection.query("SELECT product_name, stock_quantity FROM products WHERE item_id=" + Id, function (err, res) {
+
+        console.log(res[0].product_name)
+        
+        const oldStockQuantity = res[0].stock_quantity;
+        const newStockQuantity = oldStockQuantity - answers.units;
+        const array = []
+        const json = JSON.stringify(oldStockQuantity)
+        array.push(json)
+        console.log(array)
+
+        console.log(newStockQuantity,oldStockQuantity)
+
+        if (answers.units > oldStockQuantity) {
+          console.log("Insufficent Stock!")
+          connection.end();
+        }
+
+       if(oldStockQuantity>answers.units){
+        connection.query("UPDATE products SET stock_quantity=" +  newStockQuantity + " WHERE stock_quantity=" + array[0] );
+       }
+
+        
+
+        connection.end()
+      })
+
+
+    });
+  }
+});
+
+
