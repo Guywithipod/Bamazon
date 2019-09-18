a63da16ee5344a9baa95082c7b91d8a84a4956c9
Anthony Dominguez <Anthonydv7@outlook.com>
2019-09-18 17:14:23 -0500

    comments added into the code for clarity

--- bamazon.js
+++ bamazon.js
@@ -1,7 +1,5 @@
 const inquirer = require("inquirer");
-
 const mysql = require("mysql");
-
 const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -9,14 +7,13 @@ const connection = mysql.createConnection({
   password: "root",
   database: "bamazon"
 });
-
+// here is where we first console log all the information to the terminal for the customer's sake.
 connection.query("SELECT item_id, product_name, price FROM products", function (err, res) {
   if (err) throw err;
   for (let i = 0; i < res.length; i++) {
     console.log("Item ID:" + res[i].item_id + "     Product Name:" + res[i].product_name + "        Price:" + res[i].price);
-
   }
-
+  // after the customer has browsed for a bit he/she is promped two questions.
   inquirer.prompt([
     {
       name: "name",
@@ -28,46 +25,37 @@ connection.query("SELECT item_id, product_name, price FROM products", function (
       message: "How many units do you wish to purchuse?"
     },
   ]).then(function (answers) {
-
-    readtable(answers)
+    readtable(answers);
   });
 
-
+  // here is where the customers answers are recorded and are used to interact with mySQL.
   function readtable(answers) {
     connection.query("SELECT item_id FROM products", function (err, res) {
       if (err) throw err;
-
-      const Id = answers.name
-      console.log(Id)
+      const Id = answers.name;
+      console.log(Id);
 
       connection.query("SELECT product_name, stock_quantity FROM products WHERE item_id=" + Id, function (err, res) {
-
-        console.log(res[0].product_name)
-        
+// here the old stock quantity of the item they purchused is turned into a string. Also the new stock quantity is calcuted here as well.
+        console.log(res[0].product_name);
         const oldStockQuantity = res[0].stock_quantity;
         const newStockQuantity = oldStockQuantity - answers.units;
-        const array = []
-        const json = JSON.stringify(oldStockQuantity)
-        array.push(json)
-        console.log(array)
-
-        console.log(newStockQuantity,oldStockQuantity)
-
+        const array = [];
+        const json = JSON.stringify(oldStockQuantity);
+        array.push(json);
+        console.log(array);
+        console.log(newStockQuantity, oldStockQuantity);
+// If there is not enough stock quantity then the msg "Insufficent Stock!" is displayed.
         if (answers.units > oldStockQuantity) {
-          console.log("Insufficent Stock!")
+          console.log("Insufficent Stock!");
           connection.end();
         }
-
-       if(oldStockQuantity>answers.units){
-        connection.query("UPDATE products SET stock_quantity=" +  newStockQuantity + " WHERE stock_quantity=" + array[0] );
-       }
-
-        
-
-        connection.end()
+// if there is enough stock quantity then the SQL data base is updated with the new stock quantity.
+        if (oldStockQuantity > answers.units) {
+          connection.query("UPDATE products SET stock_quantity=" + newStockQuantity + " WHERE stock_quantity=" + array[0]);
+        }
+        connection.end();
       })
-
-
     });
   }
 });
